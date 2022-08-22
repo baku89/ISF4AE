3a125b7cb97fe6a4211fb31cc5f4a916936fce70
Baku Hashimoto <mail@baku89.com>
2022-08-22 19:14:13 +0900

    Implement WeakMap

---
+++ Headers/WeakMap.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <unordered_map>
+#include <memory>
+
+using namespace std;
+
+/**
+ * Emulates EcmaScripts' WeakMap in C++
+ */
+template <class K, class V>
+class WeakMap {
+private:
+    unordered_map<K, weak_ptr<V>> _map;
+    
+public:
+    shared_ptr<V> get(const K &key);
+    bool has(const K &key);
+    void set(const K &key, const shared_ptr<V> &value);
+    
+};
