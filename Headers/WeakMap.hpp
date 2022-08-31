1bf179190bc0568df24e12444db1326d81efe4bd
Baku Hashimoto <mail@baku89.com>
2022-08-31 23:44:40 +0900

    Merge the hpp & cpp of WeakMap

--- Headers/WeakMap.hpp
+++ Headers/WeakMap.hpp
@@ -14,7 +14,22 @@ class WeakMap {
   unordered_map<K, weak_ptr<V>> _map;
 
  public:
-  shared_ptr<V> get(const K& key);
-  bool has(const K& key);
-  void set(const K& key, const shared_ptr<V>& value);
+  shared_ptr<V> get(const K& key) {
+    if (!has(key)) {
+      return nullptr;
+    }
+    return _map[key].lock();
+  };
+
+  bool has(const K& key) {
+    if (_map.find(key) == _map.end()) {
+      return false;
+    }
+
+    weak_ptr<V> value = _map[key];
+
+    return !value.expired();
+  };
+
+  void set(const K& key, const shared_ptr<V>& value) { _map[key] = weak_ptr<V>(value); };
 };
