b1189faf2ed1fb291df845a75afc0441b57be61f
Baku Hashimoto <mail@baku89.com>
2022-09-02 23:54:06 +0900

    Update the version to v0.1.1

--- Config.h
+++ Config.h
@@ -22,6 +22,6 @@
 
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1
-#define BUG_VERSION 0
+#define BUG_VERSION 1
 #define STAGE_VERSION PF_Stage_DEVELOP
-#define BUILD_VERSION 1
+#define BUILD_VERSION 0
