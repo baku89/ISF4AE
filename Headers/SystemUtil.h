08e660f1466fa8b2210c8110e290b7130fa0dfd7
Baku Hashimoto <mail@baku89.com>
2022-09-10 02:09:02 +0900

    Add some new options for open/saveFileDialog

--- Headers/SystemUtil.h
+++ Headers/SystemUtil.h
@@ -5,8 +5,12 @@
 
 namespace SystemUtil {
 
-std::string openFileDialog(const std::vector<std::string>& fileTypes);
-std::string saveFileDialog(const std::string& filename);
+std::string openFileDialog(const std::vector<std::string>& fileTypes,
+                           const std::string& directory = "",
+                           const std::string& title = "");
+std::string saveFileDialog(const std::string& filename,
+                           const std::string& directory = "",
+                           const std::string& title = "");
 
 std::string readTextFile(const std::string& path);
 bool writeTextFile(const std::string& path, const std::string& text);
